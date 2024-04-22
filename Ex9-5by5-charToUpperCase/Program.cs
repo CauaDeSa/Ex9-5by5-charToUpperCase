cabbc55ea0bfffca3f7f795d3076dec6badb1c38
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-22 14:55:33 -0300

    Add project files.

---
+++ Ex9-5by5-charToUpperCase/Program.cs
@@ -0,0 +1,46 @@
+﻿int size = 10;
+char[] palavra = new char[size];
+
+Console.WriteLine("\nCharactere input");
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"\nType the {i + 1}° charactere: ");
+    palavra[i] = char.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < size; i++)
+{
+    switch (palavra[i])
+    {
+        case 'a':
+            palavra[i] = 'A';
+            break;
+
+        case 'e':
+            palavra[i] = 'E';
+            break;
+
+        case 'i':
+            palavra[i] = 'I';
+            break;
+
+        case 'o':
+            palavra[i] = 'O';
+            break;
+
+        case 'u':
+            palavra[i] = 'U';
+            break;
+    }
+}
+
+Console.WriteLine("\nTo Upper Case");
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"{palavra[i]} ");
+}
+
+Console.WriteLine("\n\nPress any key to continue...");
+Console.ReadKey();
